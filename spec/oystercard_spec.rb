0b9c8dc9be292f4e40c7637b3b305fdbeef3b391
Stuart Cooper <stucooper86@gmail.com>
2016-10-03 16:05:39 +0100

    Adding oystercard balance test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
-describe 'Oystercard' do
-  
+require 'oystercard'
+
+describe Oystercard do
+
+  it { is_expected.to respond_to :balance }
+
+  it 'takes a new card and checks it has a balance' do
+    expect(subject.balance).to eq 0
+  end
+
 end
